5d0ecb948204f7c70039ca0cd6a6ebe2e7724be2
smit-vekariya-us <smit.vekariya@logicwind.com>
2026-01-15 19:48:00 +0530

    refactor: faq, html & quote component ui

--- src/components/blocks/html.tsx
+++ src/components/blocks/html.tsx
@@ -1,5 +1,10 @@
 import { HTML as HTMLProps } from '@/payload-types'
 
 export default function HTML(props: HTMLProps) {
-  return <div>HTML</div>
+  return (
+    <div className="flex flex-col gap-2 p-4 border-b">
+      <span>HTML</span>
+      <code>{JSON.stringify(props)}</code>
+    </div>
+  )
 }
--- src/components/blocks/quote.tsx
+++ src/components/blocks/quote.tsx
@@ -1,5 +1,10 @@
 import { Quote as QuoteProps } from '@/payload-types'
 
 export default function Quote(props: QuoteProps) {
-  return <div>Quote</div>
+  return (
+    <div className="flex flex-col gap-2 p-4 border-b">
+      <span>Quote</span>
+      <code>{JSON.stringify(props)}</code>
+    </div>
+  )
 }
